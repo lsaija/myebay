adfcd1d39e6c02b967f82b7026944181f7f5d1a3
lsaija <lollosaija@gmail.com>
2022-11-20 14:50:55 +0100

    implementazione model

---
+++ src/main/java/it/prova/myebay/model/Categoria.java
@@ -0,0 +1,70 @@
+package it.prova.myebay.model;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "codice")
+	private String codice;
+	
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "categorie")
+	private Set<Annuncio> annunci = new HashSet<>();
+
+	public Categoria(String descrizione, String codice) {
+		this.descrizione = descrizione;
+		this.codice = codice;
+	}
+
+	public Categoria(Long id, String descrizione, String codice) {
+		this.id = id;
+		this.descrizione = descrizione;
+		this.codice = codice;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public Set<Annuncio> getAnnunci() {
+		return annunci;
+	}
+
+	public void setAnnunci(Set<Annuncio> annunci) {
+		this.annunci = annunci;
+	}
+
+}
